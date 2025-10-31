ed0b19357bbc1b57c1687725a4ecc334045edae4
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-10-31 16:16:52 +0530

    ci: implemented build go action

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/common-nighthawk/go-figure"
 	"github.com/nakul-krishnakumar/gt-actions-tut/internal"
 )
 
 func main() {
 	adder := adder.New(8, 9)
 
-	fmt.Printf("Output: %d", adder.GetResult())
+	fmt.Printf("Output: %d\n", adder.GetResult())
+
+	myFigure := figure.NewFigure("Hello World", "", true)
+  	myFigure.Print()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nakul-krishnakumar/gt-actions-tut
 
 go 1.24.5
+
+require github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
